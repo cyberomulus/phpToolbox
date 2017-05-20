590cd1dc000da6488ad2256767173fab30dfde78
Romain Brack (cyberomulus) <me@cyberomulus.me>
2017-05-20 23:01:02 +0200

    add test for copyFS and reorganize test for dependencies

--- tests/IOTest.php
+++ tests/IOTest.php
@@ -27,28 +27,31 @@ class IOTest extends TestCase
 	protected $ioClass;
 
 	/**
-	 * create the fixtures
-	 */
-	public static function setUpBeforeClass()
-		{
-		// create a directory for test rmdir function
-		if (file_exists("tests/fixtures/io_rmDirFS/tested") == false)
-			{
-			mkdir("tests/fixtures/io_rmDirFS/tested");
-			touch("tests/fixtures/io_rmDirFS/tested/file.txt");
-			touch("tests/fixtures/io_rmDirFS/tested/file2.txt");
-			mkdir("tests/fixtures/io_rmDirFS/tested/sub_category");
-			touch("tests/fixtures/io_rmDirFS/tested/sub_category/file3.txt");
-			touch("tests/fixtures/io_rmDirFS/tested/sub_category/file4.txt");
-			}
-		}
-
-	/**
 	 * Initialize the tested class
 	 */
 	protected function setUp()
 		{
 		$this->ioClass = new IO();
+		}
+
+	/**
+	 * Test if the exception has returned in the metod IO::scanDirFS($directory, $recursive)
+	 * if a directory not exist
+	 */
+	public function testScanDirFS_exceptionNotExist()
+		{
+		$this->expectExceptionMessage("The directory null not exist");
+		$this->ioClass->scanDirFS("null");
+		}
+
+	/**
+	 * Test if the exception has returned in the metod IO::scanDirFS($directory, $recursive)
+	 * if a directory is not directory
+	 */
+	public function testScanDirFS_exceptionNotDirectory()
+		{
+		$this->expectExceptionMessage("tests/fixtures/io_scanDirFS/file.txt is not a directory");
+		$this->ioClass->scanDirFS("tests/fixtures/io_scanDirFS/file.txt");
 		}
 
 	/**
@@ -79,35 +82,18 @@ class IOTest extends TestCase
 		}
 
 	/**
-	 * Test if the exception has returned in the metod IO::scanDirFS($directory, $recursive)
-	 * if a directory not exist
-	 */
-	public function testScanDirFS_exceptionNotExist()
-		{
-		$this->expectExceptionMessage("The directory null not exist");
-		$this->ioClass->scanDirFS("null");
-		}
-
-	/**
-	 * Test if the exception has returned in the metod IO::scanDirFS($directory, $recursive)
-	 * if a directory is not directory
-	 */
-	public function testScanDirFS_exceptionNotDirectory()
-		{
-		$this->expectExceptionMessage("tests/fixtures/io_scanDirFS/file.txt is not a directory");
-		$this->ioClass->scanDirFS("tests/fixtures/io_scanDirFS/file.txt");
-		}
-
-	/**
-	 * Test the metod IO::scanDirFS($directory, $recursive)
+	 * Test the method IO::copyFS($source, $destination, $permissions)
 	 *
-	 * @depends testRmDirFS_exceptionNotExist
-	 * @depends testRmDirFS_exceptionNotDirectory
+	 * @depends testScanDirFS
 	 */
-	public function testRmDirFS()
+	public function testCopyFS()
 		{
-		$this->assertTrue($this->ioClass->rmDirFS("tests/fixtures/io_rmDirFS/tested"));
-		$this->assertDirectoryNotExists("tests/fixtures/io_rmDirFS/tested");
+		// test the copy
+		$this->assertTrue($this->ioClass->copyFS("tests/fixtures/io_scanDirFS", "tests/fixtures/io_copyFS"));
+
+		// test if the copy equals original
+		$this->assertEquals($this->ioClass->scanDirFS("tests/fixtures/io_scanDirFS"),
+							$this->ioClass->scanDirFS("tests/fixtures/io_copyFS") );
 		}
 
 	/**
@@ -126,7 +112,20 @@ class IOTest extends TestCase
 	 */
 	public function testRmDirFS_exceptionNotDirectory()
 		{
-		$this->expectExceptionMessage("tests/fixtures/io_rmDirFS/.gitkeep is not a directory");
-		$this->ioClass->scanDirFS("tests/fixtures/io_rmDirFS/.gitkeep");
+		$this->expectExceptionMessage("tests/fixtures/io_scanDirFS/file.txt is not a directory");
+		$this->ioClass->scanDirFS("tests/fixtures/io_scanDirFS/file.txt");
+		}
+
+	/**
+	 * Test the method IO::rmDirFS($directory, $recursive)
+	 *
+	 * @depends testRmDirFS_exceptionNotExist
+	 * @depends testRmDirFS_exceptionNotDirectory
+	 * @depends testCopyFS
+	 */
+	public function testRmDirFS()
+		{
+		$this->assertTrue($this->ioClass->rmDirFS("tests/fixtures/io_copyFS"));
+		$this->assertDirectoryNotExists("tests/fixtures/io_copyFS");
 		}
 	}
