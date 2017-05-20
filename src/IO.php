b670d28edec4742de42518ba2d831ae5c3b4b92b
Romain Brack (cyberomulus) <me@cyberomulus.me>
2017-05-20 18:27:09 +0200

    add function scanDirFS

--- src/IO.php
+++ src/IO.php
@@ -18,7 +18,77 @@ namespace Cyberomulus\PhpToolbox;
  */
 class IO
 	{
-	// todo function for scan dir
+	/**
+	 * Scan a directory in the filesystem and return an array.
+	 *
+	 * For like directory :
+	 * | directory
+	 *      | file.txt
+	 *      | file2.php
+	 *      | sub_directory
+	 *      |    | file3.xml
+	 *      |    | file4.xls
+	 *      |    | more_directory
+	 *      |    |    | file5.css
+	 *      | empty_directory
+	 *
+	 * The array returned :
+	 * [0] = "file.txt"
+	 * [1] = "file2.php"
+	 * ["subdirectory"][0] = "file3.xml"
+	 * ["subdirectory"][1] = "file4.xls"
+	 * ["subdirectory"]["more_directory"][0] = "file5.css"
+	 * ["empty_directory"] = array()
+	 *
+	 * @param	string	$directory	Directory for scan (absolute or relative path)
+	 * @param	bool	$recursive	true for recursive scan, false for list only files
+	 *
+	 * @return 	array	an array (see description for example)
+	 *
+	 * @throws	\InvalidArgumentException
+	 * 					If $directory not exist or is not directory
+	 * @throws 	\Exception
+	 * 					If $directory does not open
+	 * @author	Brack Romain <http://www.cyberomulus.me>
+	 */
+	public function scanDirFS($directory, $recursive = true)
+		{
+		// throw an exception is directory does not exist or is not a directory
+		if (file_exists($directory) == false)
+			throw new \InvalidArgumentException("The directory " . $directory . " not exist");
+		if (is_dir($directory) == false)
+			throw new \InvalidArgumentException($directory . " is not a directory");
 
-	// todo function for remove dir
+		// array returned
+		$returned = array();
+
+		// open directory
+		if ($opened = opendir($directory))
+			{
+			// for all file/directory
+			while (($file = readdir($opened)) !== false)
+				{
+				if ( ($file == '.') || ($file == '..') )
+					continue;
+
+				if (is_dir($directory . "/" . $file) == false)
+					$returned[] = $file;
+
+				else if ($recursive == true)
+					$returned[$file] =  $this->scanDirFS($directory . "/" . $file);
+				}
+
+			// close directory and return the array
+			closedir($opened);
+			return $returned;
+			}
+
+		// throw an exception if direction does not open
+		else
+			throw new \Exception("The directory " . $directory . " does not open");
+		}
+
+	// todo function for scan dir in ftp and ssh
+
+	// todo function for remove dir in fs, ftp and ssh
 	}
--- tests/IOTest.php
+++ tests/IOTest.php
@@ -27,10 +27,55 @@ class IOTest extends TestCase
 	protected $ioClass;
 
 	/**
-	 * Initialise the tested class
+	 * Initialize the tested class
 	 */
 	protected function setUp()
 		{
 		$this->ioClass = new IO();
+		}
+
+	/**
+	 * Test the metod IO::scanDirFS($directory, $recursive)
+	 */
+	public function testScanDirFS()
+		{
+		// test with recurise
+		$testedDirectory = $this->ioClass->scanDirFS("tests/fixtures/io_scanDirFS");
+		$arrayExpected = array( "file.txt",
+								"file2.xml",
+								"sub_directory" => array("file3.html",
+														"file4.php",
+														"empty_directory" => array(),
+														"more_directory" => array("file5.css")
+														)
+								);
+		$this->assertEquals($arrayExpected, $testedDirectory);
+
+		// test without recursive
+		$testedDirectory = $this->ioClass->scanDirFS("tests/fixtures/io_scanDirFS", false);
+		$arrayExpected = array( "file.txt",
+								"file2.xml"
+								);
+		$this->assertEquals($arrayExpected, $testedDirectory);
+		}
+
+	/**
+	 * Test if the exception has returned in the metod IO::scanDirFS($directory, $recursive)
+	 * if a directory not exist
+	 */
+	public function testScanDirFS_exceptionNotExist()
+		{
+		$this->expectExceptionMessage("The directory null not exist");
+		$this->ioClass->scanDirFS("null");
+		}
+
+	/**
+	 * Test if the exception has returned in the metod IO::scanDirFS($directory, $recursive)
+	 * if a directory is not directory
+	 */
+	public function testScanDirFS_exceptionNotDirectory()
+		{
+		$this->expectExceptionMessage("tests/fixtures/io_scanDirFS/file.txt is not a directory");
+		$this->ioClass->scanDirFS("tests/fixtures/io_scanDirFS/file.txt");
 		}
 	}
